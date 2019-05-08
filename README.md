cc4413a3d983d95d66ffe514b41718459b7c512d
Csaba Apagyi <csaba.apagyi@gmail.com>
2019-05-09 01:29:08 +0200

    Add manually triggered implementation

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# yes-my-issue-is-important
-A bot against stale bots
+# Yes, my issue is important
+
+#### A bot against stale bots.
+
+This GitHub OAtuh app will leave a comment in your name whenever a [stale bot](https://github.com/marketplace/stale) is about to close an issue you're subscribed to. _Let the fight begin._
+
+## Why?
+
+Issues don't go away just because you don't look. Many projects maintain a facade of good health by ignoring and auto-closing issues. Closed issues discourage outside contribution. It is often unclear whether an issue is still present without reading through the whole thread. And so on. This scenario is a nightmare for developers. A reasonable alternative would be using tags for issues that don't warrant the internal developer(s) attention.
+
+## How?
+
+The app watches notifications and acts when a bot comments a stale warning, usualy starting with "`This issue has been automatically marked as stale`".
--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 const queryString = require('query-string');
-const Octokit = require('@octokit/rest')
+const Octokit = require('@octokit/rest').plugin(require('@octokit/plugin-throttling'))
+const UrlPattern = require('url-pattern');
 const moment = require('moment');
 const axios = require('axios');
 axios.defaults.headers.post['Accept'] = 'application/json';
 
-// TODO: import plugin as throttling from '@octokit/plugin-throttling'
-
-/**
- * This is the main entrypoint to your Probot app
- * @param {import('probot').Application} app
- */
+// TODO: get rid of probot, an octokit + express app should be sufficient
 module.exports = app => {
-  // Your code here
   app.log('Yay, the app was loaded!')
 
   app.on('issues.opened', async context => {
@@ -20,6 +15,8 @@ module.exports = app => {
   })
 
   const server = app.route()
+
+  const message = "Don't close this issue please. This is automatic message by [Yes, my issue is important](https://github.com/thisismydesign/yes-my-issue-is-important) - a bot against stable bots."
 
   server.get('/login', async (req, res) => {
     // GitHub needs us to tell it where to redirect users after they've authenticated
@@ -31,15 +28,9 @@ module.exports = app => {
       scope: "notifications public_repo",
       redirect_uri: `${protocol}://${host}/${process.env.CALLBACK_URL_PATH}`
     })
-  
+
     const url = `https://github.com/login/oauth/authorize?${params}`
     res.redirect(url)
-  })
-
-  server.get('/whoami', async (req, res) => {
-    const octokit = await app.auth()
-    const { data } = await octokit.apps.getAuthenticated()
-    res.json(data)
   })
 
   server.get('/login/cb', async (req, res) => {
@@ -53,56 +44,58 @@ module.exports = app => {
 
     // Authenticate our Octokit client with the new token
     const token = response.data.access_token
-    const octokit = new Octokit()
-    octokit.authenticate({ type: 'token', token })
-  
-    // Get the currently authenticated user
-    const user = await octokit.users.getAuthenticated()
-    // console.log(user.data) // <-- This is what we want!
-    console.log(moment().subtract(2 , 'day'));
+    const octokit = new Octokit({
+      auth: 'token ' + token,
+      throttle: {
+        onRateLimit: (retryAfter, options) => {
+          console.warn(`Request quota exhausted for request ${options.method} ${options.url}`)
+    
+          if (options.request.retryCount === 0) { // only retries once
+            console.log(`Retrying after ${retryAfter} seconds!`)
+            return true
+          }
+        },
+        onAbuseLimit: (retryAfter, options) => {
+          // does not retry, only logs a warning
+          console.warn(`Abuse detected for request ${options.method} ${options.url}`)
+        }
+      }
+    })
 
-    const notifications = await octokit.activity.listNotifications({
+    const notifications_request = octokit.activity.listNotifications.endpoint.merge({
       all: true,
-      per_page: 100,
       since: moment().subtract(2 , 'day')
     })
-    console.log(notifications.data)
+    const notifications = await octokit.paginate(notifications_request)
 
-    notifications.data.forEach((notification) => {
+    notifications.forEach(async (notification) => {
       const latest_comment_url = notification.subject.latest_comment_url
-      if (!latest_comment_url) {
+      const subject_url = notification.subject.url
+      if (!latest_comment_url || !subject_url) {
         return
       }
-      console.log(latest_comment_url)
 
-      console.log(Octokit.get(latest_comment_url))
-      const comment = Octokit.get(latest_comment_url)
-      console.log(comment.data.body)
+      const comment_response = await octokit.request(latest_comment_url).catch(() => {})
+      if (!comment_response) {
+        return
+      }
+
+      const user = comment_response.data.user.login
+      const body = comment_response.data.body
+
+      if(user.match(/\[bot\]$/) && body.match(/^This issue has been automatically marked as stale/)) {
+        console.log("bing bing bing!")
+        console.log(subject_url)
+
+        const pattern = new UrlPattern('https\\://api.github.com/repos/:owner/:repo/issues/:number');
+        const issue_params = pattern.match(subject_url)
+        console.log(issue_params)
+
+        // Do _not_ enable this before go-live
+        // const comment = await octokit.issues.createComment(issue_params.merge({body: message}))
+      }
     })
 
-    // "This issue has been automatically marked as stale"
-
-    // octokit.issues.getComment({
-    //   owner,
-    //   repo,
-    //   comment_id
-    // })
-
-    // const comment = await octokit.issues.createComment({
-    //   owner
-    //   repo
-    //   body
-    //   number
-    // })
-    // console.log(comment.data)
-  
-    // Redirect after login
     res.redirect('/')
   })
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 }
