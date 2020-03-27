3fb70287a56c0a74707e27127a1ecf76e065e478
Zsolt Gömöri <zsgomoridev@icloud.com>
2020-03-27 13:40:43 +0100

    Initialize dev env

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
     "linebreak-style": ["error", (process.platform === "win32" ? "windows" : "unix")], // https://stackoverflow.com/q/39114446/2771889
     "import/prefer-default-export": 0, // https://stackoverflow.com/q/54245654/2771889
     "react/sort-comp": "off", // https://github.com/yannickcr/eslint-plugin-react/issues/1214
+    "no-console": "off", // console log is fine in the context of GitHub Actions
     "no-restricted-syntax": [ // Allow `ForOfStatement` but keep other defaults https://github.com/airbnb/javascript/issues/1271#issuecomment-548688952
       "error",
       {
--- index.js
+++ index.js
@@ -1,123 +1,55 @@
-const queryString = require("query-string");
-const Octokit = require("@octokit/rest").plugin(
-  require("@octokit/plugin-throttling")
-);
-const UrlPattern = require("url-pattern");
+const core = require("@actions/core");
+const { Octokit } = require("@octokit/action");
+const { throttling } = require("@octokit/plugin-throttling");
 const moment = require("moment");
-const axios = require("axios");
 
-axios.defaults.headers.post.Accept = "application/json";
+const devEnv = process.env.NODE_ENV === "dev";
 
-// TODO: get rid of probot, an octokit + express app should be sufficient
-module.exports = (app) => {
-  app.log("Yay, the app was loaded!");
+if (devEnv) {
+  // eslint-disable-next-line global-require, import/no-extraneous-dependencies
+  require("dotenv-safe").config();
+}
 
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    return context.github.issues.createComment(issueComment);
-  });
-
-  const server = app.route();
-
-  const message =
-    "Don't close this issue please. This is automatic message by [Yes, my issue is important](https://github.com/thisismydesign/yes-my-issue-is-important) - a bot against stable bots.";
-
-  server.get("/login", async (req, res) => {
-    // GitHub needs us to tell it where to redirect users after they've authenticated
-    const protocol = req.headers["x-forwarded-proto"] || req.protocol;
-    const host = req.headers["x-forwarded-host"] || req.get("host");
-
-    const params = queryString.stringify({
-      client_id: process.env.CLIENT_ID,
-      scope: "notifications public_repo",
-      redirect_uri: `${protocol}://${host}/${process.env.CALLBACK_URL_PATH}`,
-    });
-
-    const url = `https://github.com/login/oauth/authorize?${params}`;
-    res.redirect(url);
-  });
-
-  server.get("/login/cb", async (req, res) => {
-    // Exchange our "code" and credentials for a real token
-    const response = await axios.post(
-      "https://github.com/login/oauth/access_token",
-      {
-        client_id: process.env.CLIENT_ID,
-        client_secret: process.env.CLIENT_SECRET,
-        code: req.query.code,
-      }
-    );
-
-    // Authenticate our Octokit client with the new token
-    const token = response.data.access_token;
-    const octokit = new Octokit({
-      auth: `token ${token}`,
-      throttle: {
-        onRateLimit: (retryAfter, options) => {
-          console.warn(
-            `Request quota exhausted for request ${options.method} ${options.url}`
-          );
-
-          if (options.request.retryCount === 0) {
-            // only retries once
-            console.log(`Retrying after ${retryAfter} seconds!`);
-            return true;
-          }
-        },
-        onAbuseLimit: (retryAfter, options) => {
-          // does not retry, only logs a warning
-          console.warn(
-            `Abuse detected for request ${options.method} ${options.url}`
-          );
-        },
-      },
-    });
-
-    const notifications_request = octokit.activity.listNotifications.endpoint.merge(
-      {
-        all: true,
-        since: moment().subtract(2, "day"),
-      }
-    );
-    const notifications = await octokit.paginate(notifications_request);
-
-    notifications.forEach(async (notification) => {
-      const { latest_comment_url } = notification.subject;
-      const subject_url = notification.subject.url;
-      if (!latest_comment_url || !subject_url) {
-        return;
-      }
-
-      const comment_response = await octokit
-        .request(latest_comment_url)
-        .catch(() => {});
-      if (!comment_response) {
-        return;
-      }
-
-      const user = comment_response.data.user.login;
-      const { body } = comment_response.data;
-
-      if (
-        user.match(/\[bot\]$/) &&
-        body.match(/^This issue has been automatically marked as stale/)
-      ) {
-        console.log("bing bing bing!");
-        console.log(subject_url);
-
-        const pattern = new UrlPattern(
-          "https\\://api.github.com/repos/:owner/:repo/issues/:number"
-        );
-        const issue_params = pattern.match(subject_url);
-        console.log(issue_params);
-
-        // Do _not_ enable this before go-live
-        // const comment = await octokit.issues.createComment(issue_params.merge({body: message}))
-      }
-    });
-
-    res.redirect("/");
-  });
+const configs = {
+  retriesEnabled: true,
 };
+
+const ThrottledOctokit = Octokit.plugin(throttling);
+
+async function run() {
+  const octokit = new ThrottledOctokit({
+    throttle: {
+      onRateLimit: (retryAfter, options) => {
+        console.error(
+          `Request quota exhausted for request ${options.method} ${options.url}, number of total global retries: ${options.request.retryCount}`
+        );
+
+        console.log(`Retrying after ${retryAfter} seconds!`);
+
+        return configs.retriesEnabled;
+      },
+      onAbuseLimit: (retryAfter, options) => {
+        console.error(
+          `Abuse detected for request ${options.method} ${options.url}, retry count: ${options.request.retryCount}`
+        );
+
+        console.log(`Retrying after ${retryAfter} seconds!`);
+
+        return configs.retriesEnabled;
+      },
+    },
+  });
+
+  const notificationsRequest = octokit.activity.listNotificationsForAuthenticatedUser.endpoint.merge(
+    {
+      all: true,
+      since: moment().subtract(2, "days").toISOString(),
+    }
+  );
+
+  await octokit.paginate(notificationsRequest);
+}
+
+run().catch((err) => {
+  core.setFailed(err.message);
+});
