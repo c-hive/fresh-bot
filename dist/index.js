2ebf9c139db3420905a195e5d2794d2a03106896
thisismydesign <thisismydesign@users.noreply.github.com>
2020-04-01 21:37:24 +0000

    Update distified version

--- dist/index.js
+++ dist/index.js
@@ -460,7 +460,7 @@ const regExes = {
 const configs = {
   retriesEnabled: true,
   message:
-    "Don't close this issue please. This is automatic message by [Yes, my issue is important](https://github.com/c-hive/fresh) - a bot against stable bots.",
+    "Don't close this issue. This is automatic message by [Fresh](https://github.com/c-hive/fresh) - a bot against stable bots.",
 };
 
 function isBot(user, body) {
